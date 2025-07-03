b64bb961f756972074219d64fea2cf3b426ee9ce
Camila Lima <cml.isa.17@gmail.com>
2025-07-03 13:47:20 -0300

    adding folder structure

---
+++ src/test/java/br/com/chronus/gerenciamento/MsChronusGerenciamentoApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package br.com.chronus.gerenciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsChronusGerenciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
