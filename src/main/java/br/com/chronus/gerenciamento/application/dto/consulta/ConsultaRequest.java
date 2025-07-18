e48cf50ff8f7cd06e3bc5659ac3e2170c7125eb2
Camila Lima <cml.isa.17@gmail.com>
2025-07-17 23:13:32 -0300

    validations in the dto

--- src/main/java/br/com/chronus/gerenciamento/application/dto/consulta/ConsultaRequest.java
+++ src/main/java/br/com/chronus/gerenciamento/application/dto/consulta/ConsultaRequest.java
@@ -2,6 +2,7 @@ package br.com.chronus.gerenciamento.application.dto.consulta;
 
 import br.com.chronus.gerenciamento.application.enums.EnumStatusConsulta;
 import br.com.chronus.gerenciamento.application.enums.EnumTipoConsulta;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,12 +11,26 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class ConsultaRequest {
+
+    @NotNull(message = "O ID da consulta é obrigatório")
     private Integer idConsulta;
+
+    @NotNull(message = "O ID do paciente é obrigatório")
     private Integer idPaciente;
+
+    @NotNull(message = "O ID do profissional de saúde é obrigatório")
     private Integer idProfissionalSaude;
+
+    @NotNull(message = "A data do exame é obrigatória")
     private LocalDate dataHoraConsulta;
+
     private String observacaoConsulta;
+
+    @NotNull(message = "O status da consulta é obrigatório")
     private EnumStatusConsulta statusConsulta;
+
+    @NotNull(message = "O tipo de consulta é obrigatório")
     private EnumTipoConsulta tipoConsulta;
+
     private String motivoCancelamento;
 }
